8fe62f639a1c62a5f3d62f1754e297477a47d1d3
Bard-K <tngksmf103@gmail.com>
2024-02-24 14:25:52 +0900

    Initial commit

---
+++ src/main/java/thunderbard/dietitian/DietitianApiApplication.java
@@ -0,0 +1,13 @@
+package thunderbard.dietitian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietitianApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietitianApiApplication.class, args);
+    }
+
+}

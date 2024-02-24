ba352921a00b5781848b57367bf9e47fbb0c6cc7
Bard-K <tngksmf103@gmail.com>
2024-02-24 16:02:54 +0900

    test: 피쳐 커밋

--- src/main/java/thunderbard/dietitian/DietitianApiApplication.java
+++ src/main/java/thunderbard/dietitian/DietitianApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DietitianApiApplication {
 
+    private String txt;
+
     public static void main(String[] args) {
         SpringApplication.run(DietitianApiApplication.class, args);
     }

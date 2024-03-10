2b0c9c674fb0b820b44f7e061f6c7adc9fcadea5
Bard-K <tngksmf103@gmail.com>
2024-03-10 14:17:53 +0900

    feat: menu api crud 기본 예시 추가

--- src/main/java/thunderbard/dietitian/api/controller/MenuController.java
+++ src/main/java/thunderbard/dietitian/api/controller/MenuController.java
@@ -1,9 +1,7 @@
 package thunderbard.dietitian.api.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import thunderbard.dietitian.api.entity.Menu;
 import thunderbard.dietitian.api.service.MenuService;
 
@@ -16,9 +14,29 @@ public class MenuController {
 
     private final MenuService menuService;
 
-    @GetMapping("")
+    @GetMapping
     public List<Menu> findMenus() {
         return menuService.findMenus();
+    }
+
+    @GetMapping("/{id}")
+    public Menu getMenuById(@PathVariable Long id) {
+        return menuService.getMenuById(id);
+    }
+
+    @PostMapping
+    public Menu createMenu(@RequestBody Menu menu) {
+        return menuService.createMenu(menu);
+    }
+
+    @PutMapping("/{id}")
+    public Menu modifyMenu(@RequestBody Menu menu) {
+        return menuService.modifyMenu(menu);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMenu(@PathVariable Long id) {
+        menuService.deleteMenu(id);
     }
 
 }
--- src/main/java/thunderbard/dietitian/api/service/MenuService.java
+++ src/main/java/thunderbard/dietitian/api/service/MenuService.java
@@ -6,13 +6,59 @@ import thunderbard.dietitian.api.entity.Menu;
 import thunderbard.dietitian.api.repository.MenuRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MenuService {
     private final MenuRepository menuRepository;
 
+    /**
+     * 메뉴 목록 검색
+     * @return
+     */
     public List<Menu> findMenus() {
         return menuRepository.findAll();
+    }
+
+    /**
+     * 메뉴 조회
+     * @param id
+     * @return
+     */
+    public Menu getMenuById(Long id) {
+        Optional<Menu> optionalMenu = menuRepository.findById(id);
+        return optionalMenu.orElse(null);
+    }
+
+    /**
+     * 메뉴 등록
+     * @param menu
+     * @return
+     */
+    public Menu createMenu(Menu menu) {
+        return menuRepository.save(menu);
+    }
+
+    /**
+     * 메뉴 수정
+     * @param menuDetail
+     * @return
+     */
+    public Menu modifyMenu(Menu menuDetail) {
+        Menu menu = getMenuById(menuDetail.getId());
+
+        menu.setMenuName(menuDetail.getMenuName());
+
+        return menuRepository.save(menu);
+    }
+
+    /**
+     * 메뉴 삭제
+     * @param id
+     */
+    public void deleteMenu(Long id) {
+        Menu menu = getMenuById(id);
+        menuRepository.delete(menu);
     }
 }
